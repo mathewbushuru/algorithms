798c61e13a1794c856e5782cfaf471a888f6597b
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-01-29 13:56:51 -0800

    String operations part 2

--- data-structures/strings/cpp/stringOperations.cpp
+++ data-structures/strings/cpp/stringOperations.cpp
@@ -31,7 +31,7 @@ int main()
      * Length and capacity
      */
     size_t length = str2.length();            // 5
-    size_t capacity = str2.capacity();      // 22 - varies per compiler
+    size_t capacity = str2.capacity();      // 22 
 
     /*
      * Accessing characters
@@ -77,11 +77,67 @@ int main()
     {
         std::cout << "a is lexicographically less than b" << std::endl;            // This prints
     }
-     else 
+    else
     {
         std::cout << "a is lexicographically greater than b" << std::endl;
     }
-    std::cout << sub << std::endl;
+
+    /*
+    * Searching and finding substrings
+    */
+    // Find index of 'B' in "Matt B"
+    size_t pos = str1.find("B");            // 5
+
+    // searching non existent substring
+    pos = str1.find("Batt");            // garbage number
+
+    // to be sure you found substring, compare to npos
+    if (pos != std::string::npos)
+    {
+        std::cout << "Found substring at " << pos << std::endl;
+    }
+    else
+    {
+        std::cout << "Substring not found" << std::endl;
+    }
+
+    /*
+    * Modifying strings
+    */
+    std::string str8 = "Hello world";
+
+    // replace World with Universe
+    str8.replace(6, 5, "universe");         // Hello universe
+
+    // erase
+    str8.erase(6, 3);                           // Hello verse
+
+    // insert 
+    str8.insert(6, "multi");                    // Hello multiverse
+
+    /*
+    * Convert C++ string to C String
+    */
+    const char* cString = str8.c_str();                  // Hello multiverse
+
+    /*
+     * Iterate over String
+     */
+     // using range based loop
+    for (char c : str1)
+    {
+        std::cout << c;
+    }
+    std::cout << std::endl;                             // Matt B
+
+    // using iterator - using auto type is easier
+    for (std::string::iterator it = str8.begin(); it != str8.end(); ++it)
+    {
+        std::cout << *it << ' ';
+    }
+    std::cout << std::endl;                             //H e l l o   m u l t i v e r s e 
+
+    std::cout << cString << std::endl;  
 
     return 0;
 }
