01b7323c317a7d89669ee87fbf025ac48ac2dc14
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-03-24 14:02:08 -0700

    Binary Tree C++ implementation

--- data-structures/binary-tree/cpp/binaryTree.cpp
+++ data-structures/binary-tree/cpp/binaryTree.cpp
@@ -53,14 +53,129 @@ private:
         else
         {
             // delete current node
+            if (!node->left)
+            {
+                return node->right;
+            }
+            else if (!node->right)
+            {
+                return node->left;
+            }
+            else
+            {
+                // has two children
+                Node* minRight = node->right.get();
+                while (minRight->left)
+                {
+                    minRight = minRight->left.get();
+                }
+                node->value = minRight->value;
+                node->right = std::move(
+                    deleteNode(node->right, minRight->value)
+                );
+            }
         }
 
-        //placeholder return
         return node;
+    }
+
+    void inOrderTraversal(const std::unique_ptr<Node>& node)
+    {
+        if (node)
+        {
+            inOrderTraversal(node->left);
+            std::cout << node->value << " ";
+            inOrderTraversal(node->right);
+        }
+    }
+
+    void preOrderTraversal(const std::unique_ptr<Node>& node)
+    {
+        if (node)
+        {
+            std::cout << node->value << " ";
+            preOrderTraversal(node->left);
+            preOrderTraversal(node->right);
+        }
+    }
+
+    void postOrderTraversal(const std::unique_ptr<Node>& node)
+    {
+        if (node)
+        {
+            postOrderTraversal(node->left);
+            postOrderTraversal(node->right);
+            std::cout << node->value << " ";
+        }
+    }
+
+public:
+    void insert(int value)
+    {
+        insertNode(root, value);
+    }
+
+    void deleteValue(int value)
+    {
+        root = std::move(
+            deleteNode(root, value)
+        );
+    }
+
+    void inOrder()
+    {
+        inOrderTraversal(root);
+        std::cout << std::endl;
+    }
+
+    void preOrder()
+    {
+        preOrderTraversal(root);
+        std::cout << std::endl;
+    }
+
+    void postOrder()
+    {
+        postOrderTraversal(root);
+        std::cout << std::endl;
     }
 };
 
 int main()
 {
+    BinaryTree binaryTree;
+
+    binaryTree.insert(8);
+    binaryTree.insert(3);
+    binaryTree.insert(10);
+    binaryTree.insert(1);
+    binaryTree.insert(6);
+    binaryTree.insert(14);
+    binaryTree.insert(4);
+    binaryTree.insert(7);
+    binaryTree.insert(13);
+
+    // 1 3 4 6 7 8 10 13 14 
+    std::cout << "In order traversal" << std::endl;
+    binaryTree.inOrder();
+    std::cout << "------" << std::endl;
+
+    // 8 3 1 6 4 7 10 14 13 
+    std::cout << "Pre order traversal" << std::endl;
+    binaryTree.preOrder();
+    std::cout << "------" << std::endl;
+
+    // 1 4 7 6 3 13 14 10 8 
+    std::cout << "Post order traversal" << std::endl;
+    binaryTree.postOrder();
+    std::cout << "------" << std::endl;
+
+    binaryTree.deleteValue(6);
+
+    // 1 3 4 7 8 10 13 14 
+    std::cout << "In order traversal after deleting 6" << std::endl;
+    binaryTree.inOrder();
+    std::cout << "------" << std::endl;
+
     return 0;
 }
