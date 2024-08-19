6d0f9345d014503af4fd940d539a0591a00ef438
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-08-18 20:08:56 -0700

    Configure new next js site

---
+++ docs/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="my-8 flex flex-col items-center gap-8">
+      Data Structures and Algorithms
+      <Button>Click here</Button>
+    </div>
+  );
+}
